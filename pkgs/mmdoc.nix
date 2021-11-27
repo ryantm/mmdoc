734a5a5571ad30294a0abdb0a12e04446cb41bce
Ryan Mulligan <ryan@ryantm.com>
2021-11-27 07:50:19 -0800

    dev: update flake inputs, fix cmark-gfm'

--- pkgs/mmdoc.nix
+++ pkgs/mmdoc.nix
@@ -12,6 +12,15 @@ pkg-config
 
 with lib;
 
+let
+  cmark-gfm' = cmark-gfm.overrideAttrs (oldAttrs: rec {
+    # remove when https://github.com/github/cmark-gfm/pull/248 merged and released
+    postInstall = ''
+      substituteInPlace $out/include/cmark-gfm-core-extensions.h \
+        --replace '#include "config.h"' '#include <stdbool.h>'
+    '';
+  });
+in
 stdenv.mkDerivation rec {
   pname = "mmdoc";
   version = "0.8.0";
@@ -22,7 +31,7 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
 
-  buildInputs = [ cmark-gfm fastJson xxd libzip.dev ];
+  buildInputs = [ cmark-gfm' fastJson xxd libzip.dev ];
 
   meta = {
     description = "Minimal Markdown Documentation";
