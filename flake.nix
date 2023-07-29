fe0ff1ad8a7548d05f16908084ca03b3d3f8dea1
Ryan Mulligan <ryan@ryantm.com>
2023-07-29 09:56:44 -0500

    dev: use nix-systems/systems in flake; simplify flake; update lock

--- flake.nix
+++ flake.nix
@@ -1,34 +1,20 @@
 {
   description = "Minimal Markdown Documentation";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
+  inputs.systems.url = "github:nix-systems/default";
   outputs = {
     self,
     nixpkgs,
-  } @ args: let
-    p = system:
-      import ./pkgs/default.nix {
-        inherit (args) self nixpkgs;
-        inherit system;
-      };
+    systems,
+  }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
     overlays.default = final: prev: {mmdoc = self.packages.${prev.system}.mmdoc;};
-
-    packages.aarch64-linux = p "aarch64-linux";
-    devShells.aarch64-linux.default = self.packages.aarch64-linux.devShell;
-    formatter.aarch64-linux = self.packages.x86_64-linux.fmt;
-
-    packages.i686-linux = p "i686-linux";
-    devShells.i686-linux.default = self.packages.i686-linux.devShell;
-    formatter.i686-linux = self.packages.x86_64-linux.fmt;
-
-    packages.x86_64-darwin = p "x86_64-darwin";
-    devShells.x86_64-darwin.default = self.packages.x86_64-darwin.devShell;
-    formatter.x86_64-darwin = self.packages.x86_64-linux.fmt;
-
-    packages.x86_64-linux = p "x86_64-linux";
-    devShells.x86_64-linux.default = self.packages.x86_64-linux.devShell;
-    formatter.x86_64-linux = self.packages.x86_64-linux.fmt;
+    packages = eachSystem (system:
+      import ./pkgs/default.nix {
+        inherit self nixpkgs system;
+      });
+    devShells = eachSystem (system: {default = self.packages.${system}.devShell;});
+    formatter = eachSystem (system: self.packages.${system}.fmt);
   };
 }
