ddcb14b155c02abad1a0dc15993905d577f3c0d8
Ryan Mulligan <ryan@ryantm.com>
2023-07-30 13:49:54 -0500

    dev: fix alejandra formatter
    It was using PATH instead of the input.

--- pkgs/fmt.nix
+++ pkgs/fmt.nix
@@ -7,5 +7,5 @@ writeScriptBin "fmt" ''
   echo "Formatting C code..."
   find src -iname *.h -o -iname *.c | xargs ${clang-tools}/bin/clang-format -i
   echo "Formatting Nix code..."
-  alejandra -q .
+  ${alejandra}/bin/alejandra -q .
 ''
