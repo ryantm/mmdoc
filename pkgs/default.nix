69f307f4433538abf0ae1f8d31612dc62bc9e7e8
Ryan Mulligan <ryan@ryantm.com>
2023-09-10 12:12:11 +0200

    fix: remove unused nixpkgs-for-manual
    We're getting this from the flake input nixpkgs instead

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,12 +4,6 @@
   self,
 }: let
   pkgs = nixpkgs.legacyPackages.${system};
-  nixpkgs-for-manual = pkgs.fetchFromGitHub {
-    owner = "ryantm";
-    repo = "nixpkgs";
-    rev = "99bc88dcdbdd8f375fd3cd7395eafe55d9857283";
-    sha256 = "08q5y5j2qpxk5gpjyhbz10ghyzicbdqibfpfnxgxq8fz649r1i7w";
-  };
 in rec {
   default = mmdoc;
   mmdoc = pkgs.callPackage ./mmdoc.nix {inherit self;};
